93a6bd45ecfbe1c209ba9e45eb7411c92f9f44f0
Jdmcc890 <55810535+Jdmcc890@users.noreply.github.com>
2019-09-25 15:38:21 -0500

    Added Unity Game

---
+++ Gem Coin Thing/Assets/Scripts/Cowwectable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Cowwectable : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		HandleCollected();
+	}
+	void HandleCollected()
+	{
+		CollectibleManager.Instance.HandleCoinCollected();
+		Destroy(gameObject);
+	}
+
+}
